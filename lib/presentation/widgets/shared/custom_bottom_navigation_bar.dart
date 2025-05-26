440897d19bd14893a463ecc67097b060b91fbf49
Manuela Viola <manuela.viola@gmail.com>
2025-05-26 11:21:31 -0300

    feat: add custom bottom navigation bar - only skeleton

---
+++ lib/presentation/widgets/shared/custom_bottom_navigation_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  const CustomBottomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      elevation: 0, // para quitar linea top divisoria
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.label_outline),
+          label: 'Categorías',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite_outline),
+          label: 'Favoritos',
+        ),
+      ],
+    );
+  }
+}
