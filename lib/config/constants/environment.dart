4899e0c7cf2c7219550123a8a9b8d62896b8face
Manuela Viola <manuela.viola@gmail.com>
2025-05-25 22:34:34 -0300

    feat: implement movie mapper

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -2,7 +2,10 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
   static String language = 'es-MX';
-  static String theMovieDbUrl = dotenv.env['THE_MOVIEDB_URL'] ?? 'No hay url';
+  static String theMovieDbUrl =
+      dotenv.env['THE_MOVIEDB_URL'] ?? 'No hay url de movie db';
   static String theMovieDbKey =
-      dotenv.env['THE_MOVIEDB_KEY'] ?? 'No hay api Key';
+      dotenv.env['THE_MOVIEDB_KEY'] ?? 'No hay api Key de movie db';
+  static String imageUrl =
+      dotenv.env['THE_MOVIEDB_IMAGE_URL'] ?? 'No hay url de imagenes';
 }
---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,28 @@
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDB movie) => Movie(
+    adult: movie.adult,
+    backdropPath:
+        movie.backdropPath != ''
+            ? '${Environment.imageUrl}${movie.backdropPath}'
+            : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdSx3JMdW7BwCPy6VFIrEMZI3Q7p-pIdVedA&s',
+    genreIds: movie.genreIds.map((e) => e.toString()).toList(),
+    id: movie.id,
+    originalLanguage: movie.originalLanguage,
+    originalTitle: movie.originalTitle,
+    overview: movie.overview,
+    popularity: movie.popularity,
+    posterPath:
+        movie.posterPath != ''
+            ? '${Environment.imageUrl}${movie.posterPath}'
+            : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdSx3JMdW7BwCPy6VFIrEMZI3Q7p-pIdVedA&s',
+    releaseDate: movie.releaseDate,
+    title: movie.title,
+    video: movie.video,
+    voteAverage: movie.voteAverage,
+    voteCount: movie.voteCount,
+  );
+}
