07ee2a89b0fd5273bcf9c4c64a6de999c16b0445
Manuela Viola <manuela.viola@gmail.com>
2025-05-27 15:26:12 -0300

    feat: use get movie details

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,15 +1,43 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MovieScreen extends StatelessWidget {
+import '../../providers/providers.dart';
+
+class MovieScreen extends ConsumerStatefulWidget {
   static const name = 'movie_screen';
   final String movieId;
   const MovieScreen({super.key, required this.movieId});
 
   @override
+  MovieScreenState createState() => MovieScreenState();
+}
+
+class MovieScreenState extends ConsumerState<MovieScreen> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
+
+    if (movie == null) {
+      return Scaffold(
+        body: const Center(child: CircularProgressIndicator(strokeWidth: 3)),
+      );
+    }
+
     return Scaffold(
-      appBar: AppBar(title: Text('MovieID: $movieId')),
-      body: const Placeholder(),
+      appBar: AppBar(title: Text('MovieID: ${widget.movieId}')),
+      body: Center(child: Text(movie.title)),
     );
   }
 }
