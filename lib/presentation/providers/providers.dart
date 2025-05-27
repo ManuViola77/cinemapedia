66527b7d764a0aee778813e7f99def418e975d91
Manuela Viola <manuela.viola@gmail.com>
2025-05-27 15:17:00 -0300

    feat: movie info provider

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -2,3 +2,4 @@ export 'movies/initial_loading_provider.dart';
 export 'movies/movies_providers.dart';
 export 'movies/movies_repository_provider.dart';
 export 'movies/movies_slide_show_provider.dart';
+export 'movies/movie_info_provider.dart';
