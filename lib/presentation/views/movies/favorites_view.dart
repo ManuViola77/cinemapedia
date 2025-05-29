4000592e36966ba555eebdc5f3acb66c95306f0f
Manuela Viola <manuela.viola@gmail.com>
2025-05-29 17:57:57 -0300

    feat: favorites empty state

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:go_router/go_router.dart';
 
 class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
@@ -46,9 +47,35 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   }
 
   @override
-  Widget build(BuildContext currentContext) {
+  Widget build(BuildContext context) {
     final Map<int, Movie> favoriteMoviesMap = ref.watch(favoriteMoviesProvider);
     final List<Movie> favoriteMoviesList = favoriteMoviesMap.values.toList();
+
+    if (favoriteMoviesList.isEmpty) {
+      final colors = Theme.of(context).colorScheme;
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.favorite_outline_sharp, size: 60, color: colors.primary),
+            Text(
+              'Oh no!!',
+              style: TextStyle(fontSize: 30, color: colors.primary),
+            ),
+            const Text(
+              'No tienes peliculas favoritas',
+              style: TextStyle(fontSize: 20, color: Colors.black45),
+            ),
+            const SizedBox(height: 20),
+            FilledButton.tonal(
+              onPressed: () => context.go('/home/0'),
+              child: const Text('Empieza a buscar'),
+            ),
+          ],
+        ),
+      );
+    }
 
     return Scaffold(
       body: MovieMasonry(
