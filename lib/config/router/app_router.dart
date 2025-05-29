1643ffe4aed827bda8bbfe4ecf72304ba31b4458
Manuela Viola <manuela.viola@gmail.com>
2025-05-28 23:54:06 -0300

    refactor: use shell route for routes

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -5,7 +5,36 @@ import 'package:go_router/go_router.dart';
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
+    ShellRoute(
+      builder: (context, state, child) => HomeScreen(childView: child),
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) {
+            return const HomeView();
+          },
+          routes: [
+            GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id';
+                return MovieScreen(movieId: movieId);
+              },
+            ),
+          ],
+        ),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        ),
+      ],
+    ),
+
+    // rutas padre/hijo
+    /*  GoRoute(
       path: '/',
       name: HomeScreen.name,
       // TODO: send correct child view
@@ -20,6 +49,6 @@ final appRouter = GoRouter(
           },
         ),
       ],
-    ),
+    ), */
   ],
 );
