e97ba57d678a1f0f8229400818b8a200e7c88925
Manuela Viola <manuela.viola@gmail.com>
2025-05-29 00:26:37 -0300

    feat: add pageIndex to home screen

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -5,13 +5,20 @@ import '../../views/views.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home_screen';
+  final int pageIndex;
 
-  const HomeScreen({super.key});
+  const HomeScreen({super.key, required this.pageIndex});
+
+  final viewRoutes = const <Widget>[
+    HomeView(),
+    SizedBox(), // <--- categorias view
+    FavoritesView(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HomeView(),
+    return Scaffold(
+      body: IndexedStack(index: pageIndex, children: viewRoutes),
       bottomNavigationBar: CustomBottomNavigationBar(),
     );
   }
