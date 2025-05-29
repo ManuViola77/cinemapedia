2d4724f42dee22400a20d9c104bd9a6608d288b1
Manuela Viola <manuela.viola@gmail.com>
2025-05-29 13:00:27 -0300

    feat: configure isar instance

--- lib/infrastructure/datasources/isar_datasource_impl.dart
+++ lib/infrastructure/datasources/isar_datasource_impl.dart
@@ -1,7 +1,30 @@
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
 import 'package:cinemapedia/domain/datasources/local_storage_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
 class IsarDatasourceImpl extends LocalStorageDatasource {
+  late Future<Isar> db;
+
+  IsarDatasourceImpl() {
+    db = openDB();
+  }
+
+  Future<Isar> openDB() async {
+    final dir = await getApplicationDocumentsDirectory();
+
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open(
+        [MovieSchema],
+        inspector: true,
+        directory: dir.path,
+      );
+    }
+
+    return Future.value(Isar.getInstance());
+  }
+
   @override
   Future<List<Movie>> getFavoriteMovies({int limit = 10, int offset = 0}) {
     // TODO: implement getFavoriteMovies
