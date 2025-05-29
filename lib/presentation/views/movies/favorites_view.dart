e48eba335c34f1b929cdbed8972ce638dc22e6cc
Manuela Viola <manuela.viola@gmail.com>
2025-05-29 16:58:29 -0300

    feat: show favorites movies without design

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext currentContext) {
+    final Map<int, Movie> favoriteMoviesMap = ref.watch(favoriteMoviesProvider);
+    final List<Movie> favoriteMoviesList = favoriteMoviesMap.values.toList();
+
     return Scaffold(
-      appBar: AppBar(title: Text('Favorites View')),
-      body: Center(child: Text('Favoritos')),
+      body: ListView.builder(
+        itemCount: favoriteMoviesList.length,
+        itemBuilder: (context, index) {
+          final movie = favoriteMoviesList[index];
+          return ListTile(title: Text(movie.title));
+        },
+      ),
     );
   }
 }
