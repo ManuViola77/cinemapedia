905a05fbed16cb268393d8825d46251a2827d4bd
Manuela Viola <manuela.viola@gmail.com>
2025-05-29 12:50:24 -0300

    feat: create datasource and repository

---
+++ lib/infrastructure/datasources/isar_datasource_impl.dart
@@ -0,0 +1,22 @@
+import 'package:cinemapedia/domain/datasources/local_storage_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+class IsarDatasourceImpl extends LocalStorageDatasource {
+  @override
+  Future<List<Movie>> getFavoriteMovies({int limit = 10, int offset = 0}) {
+    // TODO: implement getFavoriteMovies
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> isMovieFavorite(int movieId) {
+    // TODO: implement isMovieFavorite
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) {
+    // TODO: implement toggleFavorite
+    throw UnimplementedError();
+  }
+}
---
+++ lib/infrastructure/repositories/local_storage_repository_impl.dart
@@ -0,0 +1,27 @@
+import 'package:cinemapedia/domain/datasources/local_storage_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/local_storage_repository.dart';
+
+class LocalStorageRepositoryImpl extends LocalStorageRepository {
+  final LocalStorageDatasource datasource;
+
+  LocalStorageRepositoryImpl(this.datasource);
+
+  @override
+  Future<List<Movie>> getFavoriteMovies({
+    int limit = 10,
+    int offset = 0,
+  }) async {
+    return datasource.getFavoriteMovies(limit: limit, offset: offset);
+  }
+
+  @override
+  Future<bool> isMovieFavorite(int movieId) {
+    return datasource.isMovieFavorite(movieId);
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) {
+    return datasource.toggleFavorite(movie);
+  }
+}
