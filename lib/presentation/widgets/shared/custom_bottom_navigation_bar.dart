8c3886efc246670babf6b557d7ea1e8418622c0c
Manuela Viola <manuela.viola@gmail.com>
2025-05-29 00:05:59 -0300

    feat: finish bottom tab bar with go router solution

--- lib/presentation/widgets/shared/custom_bottom_navigation_bar.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation_bar.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
   const CustomBottomNavigationBar({super.key});
 
+  int getCurrentIndex(BuildContext context) {
+    final location = GoRouterState.of(context).matchedLocation;
+    switch (location) {
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
+  void onItemTapped(BuildContext context, int tabIndex) {
+    switch (tabIndex) {
+      case 0:
+        context.go('/');
+      case 1:
+        context.go('/categories');
+      case 2:
+        context.go('/favorites');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      elevation: 0, // para quitar linea top divisoria
+      elevation: 0,
+      currentIndex: getCurrentIndex(context),
+      onTap: (tabIndex) {
+        onItemTapped(context, tabIndex);
+      }, // para quitar linea top divisoria
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
         BottomNavigationBarItem(
